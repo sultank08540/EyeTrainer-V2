497716d7cf8a9701f383329182144d01bfde3547
sultank08540 <sultank08540@gmail.com>
2026-09-17 11:19:56 -0400

    Add adjustable height to left-right routine

--- src/Dot/Routines/LeftRightRoutine.ts
+++ src/Dot/Routines/LeftRightRoutine.ts
@@ -2,11 +2,17 @@ import { Dot } from "../Dot.js";
 import { IDotRoutine } from "../IDotRoutine.js";
 
 let lastPhaseAngle:number = 0;
+let horizontalHeight:number = 50;
+
+window.addEventListener('Game:VerticalPositionChanged', (event: Event) => {
+  horizontalHeight = parseFloat((event as CustomEvent).detail.height);
+});
+
 export const LeftRightRoutine : IDotRoutine = {
   Execute: function(dot: Dot) {
     const newPhaseAngle:number = dot.dTime * dot.velocity + lastPhaseAngle;
     dot.X = dot.halfScreen + (dot.range * Math.cos(newPhaseAngle));
-    dot.Y = dot.halfScreen;
+    dot.Y = 100 - horizontalHeight;
     lastPhaseAngle = newPhaseAngle;
   },
   title: 'Left Right Left',
