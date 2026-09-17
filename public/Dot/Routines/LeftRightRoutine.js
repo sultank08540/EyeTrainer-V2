c1d90cb55d3f829fa4c474e915f7b072eee19550
sultank08540 <sultank08540@gmail.com>
2026-09-17 11:21:18 -0400

    Build adjustable left-right height

--- public/Dot/Routines/LeftRightRoutine.js
+++ public/Dot/Routines/LeftRightRoutine.js
@@ -1,9 +1,13 @@
 let lastPhaseAngle = 0;
+let horizontalHeight = 50;
+window.addEventListener('Game:VerticalPositionChanged', (event) => {
+    horizontalHeight = parseFloat(event.detail.height);
+});
 export const LeftRightRoutine = {
     Execute: function (dot) {
         const newPhaseAngle = dot.dTime * dot.velocity + lastPhaseAngle;
         dot.X = dot.halfScreen + (dot.range * Math.cos(newPhaseAngle));
-        dot.Y = dot.halfScreen;
+        dot.Y = 100 - horizontalHeight;
         lastPhaseAngle = newPhaseAngle;
     },
     title: 'Left Right Left',
